233f693239edea28e5f885c5ca6acf4e47a01636
Swapnil <swapnilshivpuje@gmail.com>
2024-04-08 02:33:51 +0530

    base page ready

--- caloriesprediction/home/urls.py
+++ caloriesprediction/home/urls.py
@@ -3,5 +3,5 @@ from home import views
 
 urlpatterns = [
     path('', views.index,name='home'),
-
+    path('about/', views.about,name='home'),
 ]
--- caloriesprediction/home/views.py
+++ caloriesprediction/home/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render,HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse('This is the Home Page')
+    # return HttpResponse('This is the Home Page')
+    return render(request,'base.html')
+
+def about(request):
+    return HttpResponse('This is about Page')
